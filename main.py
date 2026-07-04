1e965c3640f44fe0b1e0e4b4753eaa45d6579ef1
hidayetzadeyusif-cell <hidayetzadeyusif@gmail.com>
2026-07-04 20:49:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
+from config import load_config, validate_config, save_config
+from telegram import TelegramBot
+
 import requests, random, os, json, time
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 
 load_dotenv()
+BOT_NAME = os.getenv("BOT_NAME", "MySecBot")
+EMAIL = os.getenv("EMAIL", "your@email.com")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "None")
+CHAT_ID = os.getenv("CHAT_ID", "None")
+LOOKBACK_DAYS = 3
 
-CONFIG_FILE = "config.json"
-
-DEFAULT_CONFIG = {
-    "ciks": []
+HEADERS = {
+    "User-Agent": f"{BOT_NAME} {EMAIL}"
 }
 
+telegram_bot = TelegramBot(TELEGRAM_BOT_TOKEN, CHAT_ID)
+
+config = load_config()
+validate_config(config)
+
 SEEN_FILE = "seen.json"
-new_document_exists = False
-
-def load_config():
-    config = DEFAULT_CONFIG.copy()
-
-    try:
-        with open(CONFIG_FILE, "r") as f:
-            data = json.load(f)
-
-        if not isinstance(data, dict):
-            raise ValueError("Config must be a JSON object.")
-
-        config.update(data)
-
-    except FileNotFoundError:
-        print("config.json not found, using defaults.")
-
-    except json.JSONDecodeError as e:
-        print(f"Invalid JSON: {e}")
-        print("Using defaults.")
-
-    except Exception as e:
-        print(f"Error loading config: {e}")
-        print("Using defaults.")
-
-    return config
-
-def validate_config(config):
-    if not isinstance(config["ciks"], list):
-        raise ValueError("'ciks' must be a list.")
-
-    if not all(isinstance(cik, str) for cik in config["ciks"]):
-        raise ValueError("All CIKs must be strings.")
-    
-    if not all(len(cik) == 10 for cik in config["ciks"]):
-        raise ValueError("All CIKs must be 10 digits long.")
-
 def load_seen():
     try:
         with open(SEEN_FILE, "r") as f:
@@ -59,40 +33,8 @@ def save_seen(seen):
     with open(SEEN_FILE, "w") as f:
         json.dump(list(seen), f)
 
-
-config = load_config()
-validate_config(config)
-
 seen_documents = load_seen()
-
-ALL_CIKS = tuple(config["ciks"])
-BOT_NAME = os.getenv("BOT_NAME", "MySecBot")
-EMAIL = os.getenv("EMAIL", "your@email.com")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "None")
-CHAT_ID = os.getenv("CHAT_ID", "None")
-LOOKBACK_DAYS = 3
-
-HEADERS = {
-    "User-Agent": f"{BOT_NAME} {EMAIL}"
-}
-
-
-def send_telegram_message(msg, to, token):
-    url = f"https://api.telegram.org/bot{token}/sendMessage"
-
-    data = {
-        "chat_id": to,
-        "text": msg
-    }
-
-    response = requests.post(url, data=data)
-    result = response.json()
-
-    if not result.get("ok"):
-        raise Exception(f"Telegram error: {result}")
-
-    return result
-
+new_document_exists = False
 
 def fetch_data(url):
     response = requests.get(url, headers=HEADERS, timeout=5)
@@ -102,7 +44,6 @@ def fetch_data(url):
         raise ValueError("Response is not JSON")
 
     return response.json()
-
 
 def process_data(data, cik):
     global new_document_exists
@@ -144,17 +85,12 @@ def process_data(data, cik):
     return results
 
 
-def handle_output(output, chat_id=None, bot_token=None):
-    try:
-        send_telegram_message(output, chat_id, bot_token)
-        print("Telegram message OK")
-    except Exception as e:
-        print(f"Failed to send Telegram message:\n{e}\n")
-        print(output)
-
-
 def main():
-    for cik in ALL_CIKS:
+    config_changed = telegram_bot.process_commands(config)
+    if config_changed:
+        save_config(config)
+
+    for cik in config["ciks"]:
         try:
             url = f"https://data.sec.gov/submissions/CIK{cik}.json"
 
@@ -170,11 +106,7 @@ def main():
                     f"CIK number: {cik}.\n{doc_url}"
                 )
 
-                handle_output(
-                    output,
-                    chat_id=CHAT_ID,
-                    bot_token=TELEGRAM_BOT_TOKEN
-                )
+                telegram_bot.handle_output(output)
 
             time.sleep(0.5 + random.uniform(0, 0.5))
 
